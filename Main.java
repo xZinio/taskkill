4bc73eba3a93ca37093071b223cef2528a8c1f8a
xZinio <69718740+xZinio@users.noreply.github.com>
2021-01-08 22:07:58 +0100

    Added Icons, Panels and Buttons

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package Client;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.border.Border;
 
 
@@ -9,6 +10,7 @@ import java.awt.Color;
 import java.awt.Font;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 
 public class Main {
@@ -16,26 +18,47 @@ public class Main {
     public static void main(String[] args) {
         Border border = BorderFactory.createLineBorder(Color.BLACK,3);
 
+        var logo = new ImageIcon(".//Java//Client//res//logo.jpg", "Das bist das Logo!");
+
+        ImageIcon image = new ImageIcon(".//Java//Client//res//logo.jpg", "Das bist das Logo!");
+
+        JPanel whitePanel = new JPanel();
+        whitePanel.setBackground(Color.white);
+        whitePanel.setBounds(0, 0, 250, 250);
+        //whitePanel.setOpaque(true);
+        //whitePanel.setVisible(true);
+        
         JLabel label = new JLabel();
         label.setText("Taskkiller");
         label.setHorizontalTextPosition(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(new Color(0xFFFFFF));
         label.setFont(new Font("Arial Black",Font.PLAIN,20)); //Font auswählen
         label.setIconTextGap(100); //Gap Text - Image
         label.setBorder(border);
         label.setVerticalAlignment(JLabel.TOP);
         label.setHorizontalAlignment(JLabel.CENTER);
+        label.setIcon(image);
+
+        JButton processKillerInput = new JButton();
+        processKillerInput.setBounds(200, 100, 100, 50);
+        
+        processKillerInput.setSize(200,200);
+        processKillerInput.setVisible(true);
 
         JFrame frame = new JFrame();
-        frame.setTitle("Taskkiller Win64x made by Carlos");
+        frame.setTitle("Taskkiller Win64x made by Zinio");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setSize(637, 526);
         frame.setVisible(true);
         frame.add(label);
+        //frame.add(processKillerInput);
+        //frame.add(whitePanel); //Bedeckt im Moment die gesamte Fläche
+        
 
-        ImageIcon logo = new ImageIcon(".//res//logo.jpg");
-        frame.getContentPane().setBackground(Color.WHITE);
+       
+        frame.getContentPane().setBackground(Color.BLACK);
 
         frame.setIconImage(logo.getImage());
     
