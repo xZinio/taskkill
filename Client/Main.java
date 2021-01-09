e0834a306daaa738d588c7e546647520095138d4
xZinio <69718740+xZinio@users.noreply.github.com>
2021-01-09 16:04:14 +0100

    Fixed JLabel Bug, Added another Label and Fixed JButton

--- Client/Main.java
+++ Client/Main.java
@@ -1,11 +1,9 @@
-package M;
+package Client;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
-
-
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.BorderFactory;
@@ -16,11 +14,11 @@ import javax.swing.JButton;
 public class Main {
 
     public static void main(String[] args) {
-        Border border = BorderFactory.createLineBorder(Color.BLACK,3);
+        Border border = BorderFactory.createLineBorder(Color.BLACK, 3);
 
-        var logo = new ImageIcon("logo.jpg", "Das bist das Logo!");
+        var logo = new ImageIcon(".//Java//Client//res//logo.jpg", "-");
 
-        ImageIcon image = new ImageIcon("logo.jpg", "Das bist das Logo!");
+        ImageIcon image = new ImageIcon(".//Java//Client//res//logo.jpg", "-");
 
         JPanel whitePanel = new JPanel();
         whitePanel.setBackground(Color.white);
@@ -29,45 +27,55 @@ public class Main {
         whitePanel.setVisible(true);
         
         JLabel label = new JLabel();
-        label.setText("Taskkiller");
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.TOP);
-        label.setForeground(new Color(0xFFFFFF));
-        label.setFont(new Font("Arial Black",Font.PLAIN,20)); //Font auswählen
-        label.setIconTextGap(100); //Gap Text - Image
+        label.setLocation(27, 75);
+        label.setSize(500, 150);
+        label.setForeground(Color.white);
+        label.setBackground(Color.BLACK);
+        label.setIconTextGap(100);
         label.setBorder(border);
-        label.setVerticalAlignment(JLabel.TOP);
-        label.setHorizontalAlignment(JLabel.CENTER);
         label.setIcon(image);
+        label.setOpaque(true);
+        label.setVisible(true);
+
+        JLabel label2 = new JLabel();
+        label.setText("Taskkiller");
+        label.setLocation(0, 0);
+        label.setFont(new Font("Arial Black",Font.PLAIN,20));
+        label.setForeground(Color.white);
+        label.setBackground(Color.BLACK);
+        label.setOpaque(true);
+        label.setVisible(true);
 
         JButton processKillerInput = new JButton();
-        processKillerInput.setBounds(476, 341, 65, 30);
+        processKillerInput.setBounds(390, 341, 65, 30);
         processKillerInput.setSize(65,30);
         processKillerInput.setBackground(new Color(0xE1E1E1));
         processKillerInput.setVisible(true);
+        processKillerInput.setText("Kill");
 
         JButton loopKillerInput = new JButton();
-        loopKillerInput.setBounds(510, 331, 65, 30);
+        loopKillerInput.setBounds(476, 341, 65, 30);
         loopKillerInput.setSize(65, 30);
         loopKillerInput.setVisible(true);
-        loopKillerInput.setBackground(Color.blue);
+        loopKillerInput.setBackground(new Color(0xE1E1E1));
+        loopKillerInput.setText("Loop");
+        loopKillerInput.setOpaque(true);
 
         JFrame frame = new JFrame();
         frame.setTitle("Taskkiller Win64x made by Zinio");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setResizable(false);
+        frame.add(label);
+        frame.add(label2);
+        frame.add(processKillerInput);
+        frame.add(loopKillerInput);
+        frame.add(whitePanel);
         frame.setSize(637, 526);
         frame.setVisible(true);
-        frame.add(label);
-        frame.add(processKillerInput);
-        frame.add(whitePanel);
         
-
-       
+        
         frame.getContentPane().setBackground(Color.BLACK);
-
         frame.setIconImage(logo.getImage());
-    
     }
 }
