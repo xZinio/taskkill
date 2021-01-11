9f46734870c8886fc5c93b0c8de647caff71905f
Frddx <77174866+Frddx@users.noreply.github.com>
2021-01-11 11:30:20 +0100

    Added 2nd JPanel + 2 JButtons

--- Client/Main.java
+++ Client/Main.java
@@ -1,4 +1,4 @@
-package src;
+package Client;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,15 +17,21 @@ public class Main {
     public static void main(String[] args) {
         Border border = BorderFactory.createLineBorder(Color.BLACK, 3);
 
-        var logo = new ImageIcon(".//lib//logo.jpg", "-");
+        var logo = new ImageIcon("logo.jpg", "-");
 
-        ImageIcon image = new ImageIcon(".//lib//logo.jpg", "-");
+        ImageIcon image = new ImageIcon("logo.jpg", "-");
 
         JPanel whitePanel = new JPanel();
         whitePanel.setBackground(Color.white);
         whitePanel.setBounds(59, 360, 500, 62);
         whitePanel.setOpaque(true);
         whitePanel.setVisible(true);
+
+        JPanel whitePanel2 = new JPanel();
+        whitePanel2.setBackground(Color.white);
+        whitePanel2.setBounds(59, 290, 500, 62);
+        whitePanel2.setOpaque(true);
+        whitePanel2.setVisible(true);
         
         JLabel label = new JLabel();
         label.setLocation(200, 100);
@@ -54,7 +60,13 @@ public class Main {
         processKillerInput.setBackground(new Color(0xE1E1E1));
         processKillerInput.setVisible(true);
         processKillerInput.setText("Kill");
-        processKillerInput.setFocusable(false);
+
+        JButton processKillerInput2 = new JButton();
+        processKillerInput2.setBounds(390, 306, 65, 30);
+        processKillerInput2.setSize(65,30);
+        processKillerInput2.setBackground(new Color(0xE1E1E1));
+        processKillerInput2.setVisible(true);
+        processKillerInput2.setText("Kill");
 
         JButton loopKillerInput = new JButton();
         loopKillerInput.setBounds(476, 376, 65, 30);
@@ -63,10 +75,20 @@ public class Main {
         loopKillerInput.setBackground(new Color(0xE1E1E1));
         loopKillerInput.setText("Loop");
         loopKillerInput.setOpaque(true);
-        loopKillerInput.setFocusable(false);
+
+        JButton loopKillerInput2 = new JButton();
+        loopKillerInput2.setBounds(476, 306, 65, 30);
+        loopKillerInput2.setSize(65, 30);
+        loopKillerInput2.setVisible(true);
+        loopKillerInput2.setBackground(new Color(0xE1E1E1));
+        loopKillerInput2.setText("Loop");
+        loopKillerInput2.setOpaque(true);
 
         JTextField textField = new JTextField();
         textField.setBounds(75, 376, 300, 30);
+
+        JTextField textField2 = new JTextField();
+        textField2.setBounds(75, 306, 300, 30);
 
         JFrame frame = new JFrame();
         frame.setTitle("Taskkiller Win64x made by Zinio");
@@ -76,9 +98,13 @@ public class Main {
         frame.add(label);
         frame.add(label2);
         frame.add(processKillerInput);
+        frame.add(processKillerInput2);
         frame.add(loopKillerInput);
+        frame.add(loopKillerInput2);
         frame.add(whitePanel);
+        frame.add(whitePanel2);
         frame.add(textField);
+        frame.add(textField2);
         frame.setSize(637, 526);
         frame.setVisible(true);
         
